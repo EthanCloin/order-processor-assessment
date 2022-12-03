e23b641a56bf0b24e69f1b05e1a48666cd1e67ec
Ethan Cloin <psnethan@gmail.com>
2022-12-03 12:49:54 -0500

    move schema validation into middleware fxn

--- src/routes/order-route.ts
+++ src/routes/order-route.ts
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import { Router, RequestHandler } from "express";
 import { ValidationError } from "runtypes";
 import { Order } from "../models/order";
 
@@ -11,20 +11,32 @@ const mock_items = {
   3: 2,
 };
 
-orderRouter.post("/", (req, res, next) => {
-  // validate order data matches model
+/**
+ * Assert that order request body confirms to schema defined in models/order.Order
+ * @param req
+ * @param res
+ * @param next
+ * @returns
+ */
+const validateOrderSchema: RequestHandler = (req, res, next) => {
   try {
-    const orderObj = Order.check(req.body);
+    Order.check(req.body);
+    next();
   } catch (err) {
     if (err instanceof ValidationError) {
       res
         .status(400)
         .send("invalid order request body! " + JSON.stringify(err.details));
-      return null;
+      return;
     }
   }
+};
 
-  res.status(200).send("Got your order!");
+orderRouter.post("/", validateOrderSchema, (req, res, next) => {
+  // validate order data matches model (middleware does it first)
+  const orderObj = Order.check(req.body);
+
+  res.status(200).send("got your order :) ");
   // fetch item prices from db by id
   // calc subtotal
   // add tax to calc total
