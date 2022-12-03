7e5ae52d9cea1e05c859eb5c5d2b4f9f0a452dc7
Ethan Cloin <psnethan@gmail.com>
2022-12-03 11:37:42 -0500

    outline gameplan for order post route

--- src/routes/order-route.ts
+++ src/routes/order-route.ts
@@ -2,6 +2,16 @@ import { Router } from "express";
 
 export const orderRouter = Router();
 
-orderRouter.get("/", (req, res, next) => {
-  res.send("got it :)");
+const mock_items = {
+  1: 5,
+  2: 8,
+  3: 2,
+};
+
+orderRouter.post("/", (req, res, next) => {
+  // validate order data matches model
+  // fetch item prices from db by id
+  // calc subtotal
+  // add tax to calc total
+  // return subtotal and total
 });
